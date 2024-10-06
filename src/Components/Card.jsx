88cd0ec96114e93da7b219b89f24cc63484bc730
AbuDzarAlGhifari <rasduba@gmail.com>
2024-10-06 21:14:08 +0700

    update: loading style

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,19 +2,6 @@ import React, { useState, useRef } from 'react';
 import ColorThief from 'colorthief';
 import typeColors from '../services/data';
 
-const SkeletonCard = () => (
-  <div className="animate-pulse flex items-center justify-center text-center border border-gray-500 bg-blue-gray-50 rounded-lg mb-2 sm:mb-4 px-1.5 sm:px-6 lg:px-8">
-    <div>
-      <div className="w-10 h-10 bg-gray-300 rounded-md sm:w-24 lg:w-36 sm:h-24 lg:h-36"></div>
-      <div className="w-16 h-4 mt-2 bg-gray-300 rounded"></div>
-      <div className="flex justify-center mt-1">
-        <div className="w-10 h-4 mx-1 bg-gray-300 rounded-lg"></div>
-        <div className="w-10 h-4 mx-1 bg-gray-300 rounded-lg"></div>
-      </div>
-    </div>
-  </div>
-);
-
 const darkenColor = (color) => {
   const [r, g, b] = color;
   return `rgb(${Math.max(r - 50, 0)}, ${Math.max(g - 50, 0)}, ${Math.max(
@@ -23,7 +10,7 @@ const darkenColor = (color) => {
   )})`;
 };
 
-const Card = ({ pokemon, loading, infoPokemon }) => {
+const Card = ({ pokemon, infoPokemon }) => {
   const [bgColors, setBgColors] = useState({});
   const colorThief = useRef(new ColorThief());
 
@@ -39,57 +26,53 @@ const Card = ({ pokemon, loading, infoPokemon }) => {
 
   return (
     <div className="grid grid-cols-3 gap-4 sm:grid-cols-5">
-      {loading
-        ? Array.from({ length: 12 }).map((_, index) => (
-            <SkeletonCard key={index} />
-          ))
-        : pokemon.map((poke) => (
-            <div
-              key={poke.id}
-              onClick={() => infoPokemon(poke)}
-              className="flex items-center justify-center py-2 mb-2 text-center border border-gray-500 rounded-md cursor-pointer hover:border-orange-950 sm:text-lg lg:text-xl font-poppins"
-              style={{ backgroundColor: bgColors[poke.id] || 'gray' }}
-            >
-              <div>
-                <div className="flex items-center justify-center">
-                  <img
-                    src={
-                      poke.sprites?.other['official-artwork']?.front_default ||
-                      poke.sprites?.front_default
-                    }
-                    alt={poke.name}
-                    className="w-10 h-10 sm:w-24 lg:w-36 sm:h-24 lg:h-36"
-                    crossOrigin="anonymous"
-                    onLoad={(e) => handleImageLoad(e.target, poke.id)}
-                  />
-                </div>
-                <h1
-                  className="text-xs font-extrabold capitalize sm:text-lg"
-                  style={{
-                    color: darkenColor(
-                      bgColors[poke.id]
-                        ? bgColors[poke.id].match(/\d+/g).map(Number)
-                        : [128, 128, 128]
-                    ),
-                  }}
-                >
-                  {poke.name}
-                </h1>
-                <div className="flex justify-center mt-1">
-                  {poke.types.map((type, index) => (
-                    <span
-                      key={index}
-                      className={`px-2 py-1 mx-1 font-poppins font-medium text-[10px] sm:text-sm rounded-lg ${
-                        typeColors[type.type.name] || 'bg-gray-400'
-                      }`}
-                    >
-                      {type.type.name}
-                    </span>
-                  ))}
-                </div>
-              </div>
+      {pokemon.map((poke) => (
+        <div
+          key={poke.id}
+          onClick={() => infoPokemon(poke)}
+          className="flex items-center justify-center py-2 mb-2 text-center border border-gray-500 rounded-md cursor-pointer hover:border-orange-950 sm:text-lg lg:text-xl font-poppins"
+          style={{ backgroundColor: bgColors[poke.id] || 'gray' }}
+        >
+          <div>
+            <div className="flex items-center justify-center">
+              <img
+                src={
+                  poke.sprites?.other['official-artwork']?.front_default ||
+                  poke.sprites?.front_default
+                }
+                alt={poke.name}
+                className="w-10 h-10 sm:w-24 lg:w-36 sm:h-24 lg:h-36"
+                crossOrigin="anonymous"
+                onLoad={(e) => handleImageLoad(e.target, poke.id)}
+              />
             </div>
-          ))}
+            <h1
+              className="text-xs font-extrabold capitalize sm:text-lg"
+              style={{
+                color: darkenColor(
+                  bgColors[poke.id]
+                    ? bgColors[poke.id].match(/\d+/g).map(Number)
+                    : [128, 128, 128]
+                ),
+              }}
+            >
+              {poke.name}
+            </h1>
+            <div className="flex justify-center mt-1">
+              {poke.types.map((type, index) => (
+                <span
+                  key={index}
+                  className={`px-2 py-1 mx-1 font-poppins font-medium text-[10px] sm:text-sm rounded-lg ${
+                    typeColors[type.type.name] || 'bg-gray-400'
+                  }`}
+                >
+                  {type.type.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Components/PokemonDetail.jsx
+++ src/Components/PokemonDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getPokemonDetails } from '../services/api';
+import { Spinner } from '@material-tailwind/react';
 
 const PokemonDetail = () => {
   const { id } = useParams();
@@ -27,8 +28,8 @@ const PokemonDetail = () => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <div className="spinner"></div>
+      <div className="flex items-center justify-center min-h-screen">
+        <Spinner className="w-16 h-16 text-gray-900/50" />
       </div>
     );
   }
