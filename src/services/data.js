40c1bd2f0ab8f76257a3786be76f258db245e5b7
AbuDzarAlGhifari <rasduba@gmail.com>
2024-10-11 20:31:06 +0700

    add: detail

--- src/services/data.js
+++ src/services/data.js
@@ -3,6 +3,7 @@ import { FaHeart, FaShieldAlt } from 'react-icons/fa';
 import { RiSwordFill } from 'react-icons/ri';
 import { SiCodemagic } from 'react-icons/si';
 import { GiBorderedShield } from 'react-icons/gi';
+import { MdCatchingPokemon } from 'react-icons/md';
 
 export const typeColors = {
   normal: 'bg-gray-400 text-gray-800',
@@ -32,10 +33,98 @@ export const getStatIcon = {
   'special-attack': SiCodemagic,
   'special-defense': GiBorderedShield,
   speed: IoTimer,
+  speed: IoTimer,
+  'base-experience': MdCatchingPokemon,
 };
 
 export const getStatColor = {
   low: 'to-red-500',
   medium: 'to-green-500',
   high: 'to-blue-500',
+};
+
+export const getGameData = (sprites) => {
+  const gameData = [];
+
+  Object.entries(sprites?.versions || {}).forEach(
+    ([generation, versionData]) => {
+      Object.entries(versionData).forEach(([version, images]) => {
+        if (
+          images.front_default ||
+          (images.animated && images.animated.front_default)
+        ) {
+          let region = '';
+          let releaseYear = '';
+          let gameDescription = '';
+
+          switch (generation) {
+            case 'generation-i':
+              region = 'Kanto';
+              releaseYear = '1996';
+              gameDescription =
+                'The very first Pokémon games that introduced players to the world of Pokémon. Available in Red, Blue, and Yellow versions.';
+              break;
+            case 'generation-ii':
+              region = 'Johto';
+              releaseYear = '1999';
+              gameDescription =
+                'Introduced new Pokémon, breeding, and day/night cycles in Gold, Silver, and Crystal versions.';
+              break;
+            case 'generation-iii':
+              region = 'Hoenn';
+              releaseYear = '2002';
+              gameDescription =
+                'Featured a new region, abilities, and double battles in Ruby, Sapphire, and Emerald.';
+              break;
+            case 'generation-iv':
+              region = 'Sinnoh';
+              releaseYear = '2006';
+              gameDescription =
+                'Added physical and special moves along with online trading in Diamond, Pearl, and Platinum.';
+              break;
+            case 'generation-v':
+              region = 'Unova';
+              releaseYear = '2010';
+              gameDescription =
+                'Offered a new region with 3D graphics and new battle features in Black and White.';
+              break;
+            case 'generation-vi':
+              region = 'Kalos';
+              releaseYear = '2013';
+              gameDescription =
+                'Introduced Mega Evolutions and 3D models in X and Y.';
+              break;
+            case 'generation-vii':
+              region = 'Alola';
+              releaseYear = '2016';
+              gameDescription =
+                'Added regional variants and Z-moves in Sun and Moon.';
+              break;
+            case 'generation-viii':
+              region = 'Galar';
+              releaseYear = '2019';
+              gameDescription =
+                'Introduced the Galar region and Dynamaxing in Sword and Shield.';
+              break;
+            default:
+              region = 'Unknown Region';
+              releaseYear = 'Unknown Year';
+              gameDescription = 'No description available.';
+              break;
+          }
+
+          gameData.push({
+            generation,
+            version,
+            images,
+            region,
+            releaseYear,
+            gameDescription,
+          });
+        }
+      });
+    }
+  );
+
+  return gameData;
 };
